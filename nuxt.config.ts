d32273a9021ca8cf90707b30ee0f88e8a55b9d58
Sjors <sjorspieter@live.nl>
2024-08-16 10:33:10 +0200

    fix: exclude index page from redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
     modules: ["@nuxtjs/supabase"],
 
+    supabase: {
+        redirectOptions: {
+            login: "/login",
+            callback: "/confirm",
+            exclude: ["/"],
+        }
+    },
+
     devtools: { enabled: true },
 })
